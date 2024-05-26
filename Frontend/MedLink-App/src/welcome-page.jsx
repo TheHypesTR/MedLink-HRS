a6f0801e9704c17e95f0aa5db0e11d9f3a0a8318
fafafaruk <yasaromerfaruk1@gmail.com>
2024-05-27 02:06:58 +0300

    Arkaplan beyaz olmuyo imdat

--- Frontend/MedLink-App/src/welcome-page.jsx
+++ Frontend/MedLink-App/src/welcome-page.jsx
@@ -1,10 +1,17 @@
-import './welcome-page.css';
-import React from 'react';
+import "./welcome-page.css";
+import React from "react";
 
 function WelcomePage() {
-    return(
-        <p>Welcome to MedLink Hospital</p>
-    );
+  return (
+      <div class="news">
+        <div class="news-item">
+          <div class="news-content">
+            <h2>Haber Başlığı 1</h2>
+            <p>Haber içeriği 1</p>
+          </div>
+        </div>
+      </div>
+  );
 }
 
 export default WelcomePage;

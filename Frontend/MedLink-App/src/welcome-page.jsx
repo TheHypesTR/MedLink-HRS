1267f6b097a72fcde7019fa38f07407daae38938
fafafaruk <yasaromerfaruk1@gmail.com>
2024-05-27 12:23:33 +0300

    Haber güncellemeleri

--- Frontend/MedLink-App/src/welcome-page.jsx
+++ Frontend/MedLink-App/src/welcome-page.jsx
@@ -31,18 +31,40 @@ function WelcomePage() {
   };
 
   return (
+    <>
     <main className="body1">
     <section className="hero">
         <div className="hero-content">
             <h1>All for Health, Health for All</h1>
             <p>Dünya sağlık örgütünün 77. "Herkes için Sağlık" kurul toplantısı Birleşmiş Milletler önderliğinde gerçekleşiyor.</p>
-            <a href="#" className="hero-button">Detaylı Bilgi İçin</a>
+            <a href="/Blog" className="hero-button">Detaylı Bilgi İçin</a>
         </div>
         <div className="hero-image">
             <img src="./src/assets/kurul2.png" alt="Doc!" />
         </div>
     </section>
 </main>
+<div class="specialties-container">
+        <h2 class="title">Always Caring</h2>
+        <h1 class="subtitle">Our Specialties</h1>
+        <div class="grid">
+            <div class="grid-item selected">
+                <img src="path/to/icon.png" alt="Bones"/>
+                <p>Bones</p>
+            </div>
+            <div class="grid-item">
+                <img src="path/to/icon.png" alt="Neurology"/>
+                <p>Neurology</p>
+            </div>
+            <div class="grid-item">
+                <img src="path/to/icon.png" alt="Oncology"/>
+                <p>Oncology</p>
+            </div>
+            </div>
+        </div>
+
+
+</>
   );
 }
 

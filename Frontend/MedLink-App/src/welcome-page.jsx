b62152ec31626a8676480cd1e5b3115296dc28a5
fafafaruk <yasaromerfaruk1@gmail.com>
2024-05-27 10:59:24 +0300

    denemeler devam edior

--- Frontend/MedLink-App/src/welcome-page.jsx
+++ Frontend/MedLink-App/src/welcome-page.jsx
@@ -31,14 +31,12 @@ function WelcomePage() {
   };
 
   return (
-      <div class="news">
         <div class="news-item">
           <div class="news-content">
             <h2>Haber Başlığı 1</h2>
             <p>Haber içeriği 1</p>
           </div>
         </div>
-      </div>
   );
 }
 

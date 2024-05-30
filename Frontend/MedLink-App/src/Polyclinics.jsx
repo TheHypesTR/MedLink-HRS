38aa5128878ac2366eec5a83ea6faae9ba806a68
fafafaruk <yasaromerfaruk1@gmail.com>
2024-05-30 04:02:03 +0300

    Poliklinik seçildiğinde poliklinikteki doktorları görüntüleme seçeneği eklendi.

--- Frontend/MedLink-App/src/Polyclinics.jsx
+++ Frontend/MedLink-App/src/Polyclinics.jsx
@@ -1,13 +1,20 @@
 import React, { useState, useEffect } from "react";
 import config from "../config.mjs";
 import './Polyclinics.css';
+import './Doctors.css';
 import DefaultCard from "./assets/logo.png";
 
 function Polyclinics() {
     const [polyclinics, setPolyclinics] = useState([]);
+    const [selectedPolyclinic, setSelectedPolyclinic] = useState("");
+    const [doctors, setDoctors] = useState([]);
     
-    // Database'den Poliklinikleri Çeken APİ.
     useEffect(() => {
+        ShowPolyclinics();
+    }, []);
+
+    // Database'den Poliklinikleri Çeken APİ.
+    const ShowPolyclinics = () => {
         try {
             fetch(`http://localhost:${config.PORT}/polyclinic`, {
                 method: "GET",
@@ -31,31 +38,91 @@ function Polyclinics() {
             console.log(err);
         }
 
-  }, []);
+  };
+
+  // Database'den Seçili Poliklinikteki Doktorları Çeken APİ.
+    const ShowDoctors = (polyclinic) => {
+        try {
+            fetch(`http://localhost:${config.PORT}/polyclinic/${polyclinic._id}/doctor`, {
+                method: "GET",
+                credentials: "include",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            })
+            .then((response) => response.json())
+            .then((data) => {
+                console.log(data);
+                if(data.ERROR) {
+                    alert(data.ERROR);
+                    setDoctors([]);
+                }
+                if(data)
+                    setDoctors(data);
+            })
+        
+        } catch (err) {
+            console.log(err);
+        }
+    };
+
+    const selectPolyclinic = (polyclinic) => {
+        setSelectedPolyclinic(polyclinic);
+        setPolyclinics([]);
+        ShowDoctors(polyclinic);
+      };
+
+      const returnPolyclinics = () => {
+        setSelectedPolyclinic("");
+        setDoctors([]);
+        ShowPolyclinics();
+      };
 
   // Poliklinikleri Alfabetik Sıraya Göre Sıralama Yapar.
   const sortedPolyclinics = polyclinics.sort((a, b) => a.name.localeCompare(b.name));
 
-    return (
+  return (
+    <div>
         <div className="poly-container">
             {sortedPolyclinics.map((polyclinic, index) => (
-                <div className="poly-card" key={index} >
+                <div className="poly-card" key={index} onClick={() => selectPolyclinic(polyclinic)}>
                     <img className="poly-card-background" src={DefaultCard} alt="profile picture" />
                     <div className="poly-name-background-parent">
                         <div className="poly-name-background" />
-                            <div className="poly-name-wrapper">
-                                <div className="poly-name">{polyclinic.name}</div>
-                            </div>
-                            <div className="polyclinic-parent">
-                                <b className="polyclinic">{polyclinic.description}</b>
-                                <div className="frame-wrapper1">
-                            </div>
+                        <div className="poly-name-wrapper">
+                            <div className="poly-name">{polyclinic.name}</div>
+                        </div>
+                        <div className="polyclinic-parent">
+                            <b className="polyclinic">{polyclinic.description}</b>
+                            <div className="frame-wrapper1"></div>
                         </div>
                     </div>
                 </div>
             ))}
         </div>
-    );
+        
+        {selectedPolyclinic && (
+            <div className="doctors-container">
+                <button id='poliklinikDonus' onClick={returnPolyclinics}>Poliklinikleri Göster</button>
+                {doctors.map((doctor, index) => (
+                    <div className="doctors-card" key={index}>
+                        <img className="doctor-card-background" src={doctor.image || DefaultCard} alt="profile picture" />
+                        <div className="doctor-name-background-parent">
+                            <div className="doctor-name-background" />
+                            <div className="doctors-name-wrapper">
+                                <div className="doctors-name">{`${doctor.speciality} ${doctor.name}`}</div>
+                            </div>
+                            <div className="polyclinic-parent">
+                                <b className="polyclinic">{doctor.polyclinic}</b>
+                                <div className="frame-wrapper1"></div>
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        )}
+    </div>
+);
 }
 
 export default Polyclinics;
